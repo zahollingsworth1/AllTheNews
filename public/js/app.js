0503b0b0eb45771b74db4027a184dccc9c5e9a1d
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-06-15 19:07:31 -0400

    favorites Working

--- public/js/app.js
+++ public/js/app.js
@@ -4,16 +4,23 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     // $("#articles").append("<div class='div1'>  </div>")
-    $("#articles").append("<div class='div1' data-id='" + data[i]._id + "'><h5>" + data[i].title + "</h5><a >" + data[i].link + "</a></div>");
+    $("#articles").append(
+      "<div class='div1' data-id='" +
+        data[i]._id +
+        "'><h5>" +
+        data[i].title +
+        "</h5><a >" +
+        data[i].link +
+        "</a></div>"
+    );
   }
 });
-
 
 // Whenever someone clicks a p tag
 $(document).on("click", ".div1", function() {
   // Empty the notes from the note section
-  $('#myModal').modal(open);
-  
+  $("#myModal").modal(open);
+
   $(".modal-title").empty();
   $("#summary").empty();
   $("#notes").empty();
@@ -37,10 +44,20 @@ $(document).on("click", ".div1", function() {
       // An input to enter a new title
       // $("#notes").append("<input id='titleinput' class='form-control' name='title' ><br>");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput'class='form-control' placeholder='Comment here' name='body'></textarea><br>");
+      $("#notes").append(
+        "<textarea id='bodyinput'class='form-control' placeholder='Comment here' name='body'></textarea><br>"
+      );
       // A button to submit a new note, with the id of the article saved to it
-      $("#noteButton").append("<button data-id='" + data._id + "' class='btn btn-primary m-2' id='isSaved'>Add to Favorites</button>");
-      $("#noteButton").append("<button data-id='" + data._id + "' class='btn btn-primary' id='savenote'>Save Note</button>");
+      $("#noteButton").append(
+        "<button data-id='" +
+          data._id +
+          "' class='btn btn-primary m-2' id='isSaved'>Add to Favorites</button>"
+      );
+      $("#noteButton").append(
+        "<button data-id='" +
+          data._id +
+          "' class='btn btn-primary' id='savenote'>Save Note</button>"
+      );
 
       // If there's a note in the article
       if (data.note) {
@@ -79,33 +96,53 @@ $(document).on("click", "#savenote", function() {
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
-  
-  $('#myModal').modal('hide');
+
+  $("#myModal").modal("hide");
 
   // window.location.reload();
 });
 $(document).on("click", "#isSaved", function() {
   var thisId = $(this).attr("data-id");
   $.ajax({
-    method: "UPDATE",
-    url: "/favorites/" + thisId,
-    data: {
-      // Value taken from title input
-      isSaved: true,
-      // Value taken from note textarea
-    }
-  })
+    method: "POST",
+    url: "/favorites/" + thisId
+  }).then(function() {
+    // If we were able to successfully update an Article, send it back to the client
+    alert("Added to Your Favorites!!");
+    
+  });
 });
 
+$(document).on("click", "#favorites", function() {
+  $("#favorites1").modal(open);
+
+  $(".modal-title1").append("<h2>My Favorite Articles</h2>");
+
+  $.getJSON("/articles", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].isSaved === true) {
+        console.log("hello");
+        console.log(data[i].title);
+
+        $("#summary1").append(
+          "<a href='" + data[i].link + "'>" + data[i].title + "</a><br>"
+        );
+      }
+      // Display the apropos information on the page
+      // $("#articles").append("<div class='div1'>  </div>")
+    }
+  });
+});
 
 $(document).on("click", ".close", function() {
-
   $(".modal-title").empty();
+  $(".modal-title1").empty();
   $("#summary").empty();
+  $("#summary1").empty();
   $("#notes").empty();
   $("#noteButton").empty();
 
-  $('#myModal').modal('hide');
+  $("#myModal").modal("hide");
 
   // window.location.reload();
 });
--- server.js
+++ server.js
@@ -91,6 +91,19 @@ app.get("/articles", function(req, res) {
     });
 });
 
+app.get("/favorites", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
@@ -133,7 +146,7 @@ app.post("/articles/:id", function(req, res) {
 
 app.post("/favorites/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
-  db.Article.update(
+  db.Article.findOneAndUpdate(
     { _id: req.params.id },
     { $set: { isSaved: true } },
     function(error, edited) {
